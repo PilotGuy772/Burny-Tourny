4e5bc72f7810a4b3794c0ff9efcb413c1ccaac41
Laeth English <58867214+PilotGuy772@users.noreply.github.com>
2022-09-07 19:39:56 +0000

    added a wip heatsheet maker and accidentally broke the program

--- Lineup.cs
+++ Lineup.cs
@@ -6,18 +6,47 @@ class Lineup
 
     public Lineup(int evnt, string eventName, List<(Team, Team)> versuses)
     {
-        Event = evnt;
+        Event     = evnt;
         EventName = eventName;
-        Versuses = versuses;
+        Versuses  = versuses;
     }
 
     public static void CreateLineup(int e, int individualGames)
     {
-        List<Team> teams = Database.ReadTeamsByEvent(e);
+        Random rand                 = new Random();
+        List<Team> teams            = Database.ReadTeamsByEvent(e);
+        List<(Team, Team)> versuses = new List<(Team one, Team two)>();
+        (Team one, Team two) matchup;
+        int                  r1;
+        int                  r2;
+        
 
         while(teams.Count > 1)
         {
+    
+            r1 = rand.Next(0, teams.Count);
+            r2 = rand.Next(0, teams.Count);
             
+            if(r1 != r2)
+            {
+                matchup = (teams[r1], teams[r2]);
+                versuses.Add(matchup);
+                teams[r1].Games += 1;
+                teams[r2].Games += 1;
+                if(teams[r1].Games == individualGames)
+                {
+                    teams.RemoveAt(r1);
+                }else if(teams[r2].Games == individualGames)
+                {
+                    teams.RemoveAt(r2);
+                }
+            }
+            
+        }
+        Console.WriteLine("Heatsheet Generated.");
+        foreach((Team one, Team two) m in versuses)
+        {
+            Console.WriteLine($"{m.one} vs {m.two}");
         }
     }
 }
